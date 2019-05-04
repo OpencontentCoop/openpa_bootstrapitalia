be770be5e8910fb609fcf4fe30a92cf51984eead
Luca Realdi <lr@opencontent.it>
2019-05-04 11:57:53 +0200

    Corregge il file gulp perché bengano compilati anche i js (è possibile perciò eseguirne un override)

--- design/bootstrapitalia/templates/page_footer_script.tpl
+++ design/bootstrapitalia/templates/page_footer_script.tpl
@@ -1,2 +1,2 @@
 <script>window.__PUBLIC_PATH__ = "{'fonts'|ezdesign(no)}"</script>
-<script src="{'javascript/bootstrap-italia.min.js'|ezdesign(no)}"></script>
+<script src="{'javascript/app.min.js'|ezdesign(no)}"></script>
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,97 @@
 // @see https://github.com/andydkcat/customize-bootstrap-sass
 
-var
+const
     gulp = require('gulp'),
     sass = require('gulp-sass'),
-    touch = require('gulp-touch-cmd');
+    touch = require('gulp-touch-cmd'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    replace = require('gulp-replace'),
+    header = require('gulp-header'),
+    footer = require('gulp-footer'),
+    pkg = require('./package.json');
 
 // source and distribution folder
-var
+const
     source = 'src/',
     dest = 'design/bootstrapitalia/';
 
-var bootstrapItaliaSrc = {
-    in: './node_modules/bootstrap-italia/src/'
+const bootstrapItaliaSrc = {
+    in: './node_modules/bootstrap-italia/'
 };
 
-var fontAwesomeSrc = {
+const fontAwesomeSrc = {
     in: './node_modules/font-awesome/'
 };
 
-var bootstrapItaliaDist = {
-    in: './node_modules/bootstrap-italia/dist/'
-};
-
-var fonts = {
-    in: [source + 'fonts/*.*', bootstrapItaliaSrc.in + 'fonts/**/*', fontAwesomeSrc.in + 'fonts/*.*'],
+const fonts = {
+    in: [source + 'fonts/*.*', bootstrapItaliaSrc.in + 'src/fonts/**/*', fontAwesomeSrc.in + 'fonts/*.*'],
     out: dest + 'fonts/'
 };
 
-var js = {
-    in: [
-        source + 'js/*.*',
-        bootstrapItaliaDist.in + 'js/*.*',
+const js = {
+    in: [source + 'js/*.*'],
+    deps: [
         './node_modules/jquery/dist/jquery.js',
         './node_modules/popper.js/dist/umd/popper.js',
         './node_modules/owl.carousel/dist/owl.carousel.js',
-
     ],
-    out: dest + 'javascript/'
+    source: [
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './node_modules/bootstrap-select/js/bootstrap-select.js',
+        './node_modules/bootstrap-select/js/i18n/defaults-it_IT.js',
+        './node_modules/svgxuse/svgxuse.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/polyfills/array.from.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/circular-loader/CircularLoader-v1.3.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/password-strength-meter/password-strength-meter.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/datepicker/locales/it.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/datepicker/datepicker.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/i-sticky/i-sticky.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/sticky-header.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/ie.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/fonts-loader.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/autocomplete.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/back-to-top.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/componente-base.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/cookiebar.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/dropdown.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/forms.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/track-focus.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/forward.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/navbar.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/navscroll.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/history-back.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/notifications.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/upload.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/progress-donut.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/list.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/imgresponsive.js',
+        //bootstrapItaliaSrc.in + 'src/js/plugins/timepicker.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/input-number.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/carousel.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/transfer.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/select.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/rating.js',
+        bootstrapItaliaSrc.in + 'src/js/plugins/dimmer.js',
+        bootstrapItaliaSrc.in + 'src/js/bootstrap-italia.js',
+    ],
+    out: dest + 'javascript/',
 };
 
-var svg = {
-    in: [source + 'svg/*.*', bootstrapItaliaDist.in + 'svg/*.*'],
+const svg = {
+    in: [source + 'svg/*.*', bootstrapItaliaSrc.in + 'dist/svg/*.*'],
     out: dest + 'images/svg/'
 };
 
-var assets = {
-    in: [source + 'assets/*.*', bootstrapItaliaDist.in + 'assets/*.*'],
+const assets = {
+    in: [source + 'assets/*.*', bootstrapItaliaSrc.in + 'dist/assets/*.*'],
     out: dest + 'images/assets/'
 };
 
-var css = {
+const css = {
     in: source + 'scss/**/*',
     out: dest + 'stylesheets/',
     watch: source + 'scss/**/*',
@@ -57,9 +99,22 @@ var css = {
         outputStyle: 'nested',
         precision: 8,
         errLogToConsole: true,
-        includePaths: [bootstrapItaliaSrc.in + 'scss', fontAwesomeSrc.in + 'scss']
+        includePaths: [bootstrapItaliaSrc.in + 'src/scss', fontAwesomeSrc.in + 'scss']
     }
 };
+
+const jqueryCheck =
+    "if (typeof jQuery === 'undefined') {\n" +
+    "  throw new Error('Bootstrap\\'s JavaScript requires jQuery. jQuery must be included before Bootstrap\\'s JavaScript.')\n" +
+    '}\n';
+
+const jqueryVersionCheck =
+    '+function ($) {\n' +
+    "  var version = $.fn.jquery.split(' ')[0].split('.')\n" +
+    '  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {\n' +
+    "    throw new Error('Bootstrap\\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0')\n" +
+    '  }\n' +
+    '}(jQuery);\n\n';
 
 gulp.task('fonts', function () {
     return gulp
@@ -77,11 +132,54 @@ gulp.task('sass', function () {
         .pipe(touch());
 });
 
-gulp.task('js', function () {
+gulp.task('js-deps', function () {
     return gulp
-        .src(js.in)
+        .src(js.deps)
         .pipe(gulp.dest(js.out))
         .pipe(touch());
+});
+
+gulp.task('js-min', () => {
+    return gulp
+        .src(js.source)
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.init())
+        .pipe(replace(/^(export|import).*/gm, ''))
+        .pipe(
+            babel({
+                compact: true,
+                presets: [
+                    [
+                        '@babel/env',
+                        {
+                            modules: false,
+                            loose: true,
+                            exclude: ['transform-typeof-symbol'],
+                        },
+                    ],
+                ],
+                plugins: ['@babel/plugin-proposal-object-rest-spread'],
+            })
+        )
+        .pipe(uglify())
+        .pipe(
+            header(
+                jqueryCheck +
+                '\n' +
+                jqueryVersionCheck +
+                '\n+function () {\n',
+                {pkg: pkg}
+            )
+        )
+        .pipe(footer('\n}();\n'))
+        .pipe(
+            rename({
+                suffix: '.min',
+            })
+        )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(js.out))
+        .pipe(touch())
 });
 
 gulp.task('svg', function () {
@@ -99,16 +197,26 @@ gulp.task('assets', function () {
 });
 
 gulp.task(
+    'js',
+    gulp.series(
+        'js-min',
+        'js-deps'
+    )
+);
+
+gulp.task(
     'default',
     gulp.series(
         'fonts',
         'sass',
-        'js',
+        'js-min',
+        'js-deps',
         'svg',
         'assets'
     )
 );
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(css.in, gulp.series('sass'));
+    gulp.watch(js.in, gulp.series('js-min'));
 });
