acf72fd2f7bfb06fc7d8c05c171a0a5d94d8ed05
Luca Realdi <luca.realdi@opencontent.it>
2023-06-07 22:01:55 +0200

    Corregge l'icona di default nel manifest.json

--- modules/manifest.json/manifest.php
+++ modules/manifest.json/manifest.php
@@ -9,6 +9,12 @@ $data = [
             'sizes' => '512x512',
             'purpose' => 'any',
         ],
+        [
+            'src' => '/extension/openpa_bootstrapitalia/design/standard/images/svg/icon.png',
+            'type' => 'image/png',
+            'sizes' => '512x512',
+            'purpose' => 'maskable',
+        ],
     ],
     'id' => '/?source=pwa',
     'start_url' => '/?source=pwa',
