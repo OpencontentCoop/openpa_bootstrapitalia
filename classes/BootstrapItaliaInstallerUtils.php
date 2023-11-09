ab1673981f26db3a453afebeac2c7614670d287c
Luca Realdi <luca.realdi@opencontent.it>
2023-11-09 10:44:57 +0100

    Introduce una funzione per correggere il language mask delle classi in fase di installazione/aggiornamento

--- classes/BootstrapItaliaInstallerUtils.php
+++ classes/BootstrapItaliaInstallerUtils.php
@@ -23,9 +23,32 @@ class BootstrapItaliaInstallerUtils
                         $classIdentifier => [
                             'enabled' => 1,
                             'easyontology' => 'GovernmentService',
-                        ]
-                    ]
+                        ],
+                    ],
                 ]);
+            }
+        }
+    }
+
+    public static function resetClassLanguageMask()
+    {
+        /** @var eZContentClass[] $classList */
+        $query = "SELECT id FROM ezcontentclass where version = 0";
+        $idList = eZDB::instance()->arrayQuery($query);
+        foreach ($idList as $item) {
+            $class = eZContentClass::fetch((int)$item['id']);
+            $nameList = new eZContentClassNameList();
+            $nameList->initFromSerializedList($class->attribute('serialized_name_list'));
+            $originalLanguageMask = $class->attribute('language_mask');
+            foreach ($nameList->nameList() as $lang => $name) {
+                if ($lang !== 'always-available') {
+                    $class->setName($name, $lang);
+                    $languageMask = $class->attribute('language_mask');
+                }
+            }
+            if ($originalLanguageMask != $languageMask) {
+                $class->setAttribute('language_mask', $languageMask);
+                eZPersistentObject::storeObject($class);
             }
         }
     }
