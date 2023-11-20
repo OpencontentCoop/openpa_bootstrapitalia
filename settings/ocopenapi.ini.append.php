41ca2c88d90f94350addb7453cec3a9b2fe5ad4d
Luca Realdi <luca.realdi@opencontent.it>
2023-11-20 13:35:31 +0100

    no message

--- settings/ocopenapi.ini.append.php
+++ settings/ocopenapi.ini.append.php
@@ -9,4 +9,127 @@ ContentMetaPropertyFactories[privacy]=PrivacyStatusFactoryProvider
 [ClassAttributeSettings]
 ClassAttributePropertyFactories[lotto/id]=InternalIdFactoryProvider
 ClassAttributePropertyFactories[openpacomuniitaliani]=OpenPAComuniItalianiFactoryProvider
+
+[Documentation]
+Sections[]
+Sections[]=general
+Sections[]=organization
+Sections[]=politic
+Sections[]=documents
+Sections[]=services
+Sections[]=news
+Sections[]=dataset
+Sections[]=transparency
+
+[Section_general]
+title=Informazioni base
+description=Gestione delle classificazioni o archietture base del sito
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=argomenti
+tags[]=media-banner-e-link
+tags[]=media-files
+tags[]=media-immagini
+tags[]=media-images
+tags[]=media-contenuti-obsoleti
+tags[]=classificazioni-orari-uffici-e-strutture
+tags[]=classificazioni-punti-di-contatto
+tags[]=classificazioni-canali-digitali
+tags[]=classificazioni-cosa-puoi-richiedere
+tags[]=classificazioni-costi-e-tariffe
+tags[]=classificazioni-orari-servizi
+tags[]=classificazioni-estensioni-temporali-dei-dataset
+
+[Section_organization]
+title=Amministrazione e organigramma
+description=Gestione delle aree amministrative, uffici, punti di contatto, orari, personale, ruoli, enti e fondazioni
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=amministrazione-aree-amministrative
+tags[]=amministrazione-personale-amministrativo
+tags[]=amministrazione-uffici
+tags[]=classificazioni-orari-uffici-e-strutture
+tags[]=classificazioni-punti-di-contatto
+tags[]=media-ruoli
+tags[]=amministrazione-enti-e-fondazioni
+tags[]=media-immagini
+tags[]=media-images
+tags[]=argomenti
+tags[]=media-contenuti-obsoleti
+
+[Section_politic]
+title=Politici e organi di governo
+description=Gestione dei politici, organi di governo, punti di contatto, orari, ruoli
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=amministrazione-organi-di-governo
+tags[]=amministrazione-politici
+tags[]=media-ruoli
+tags[]=classificazioni-punti-di-contatto
+tags[]=amministrazione-documenti-e-dati-documenti-attivita-politica
+tags[]=media-immagini
+tags[]=media-images
+tags[]=argomenti
+tags[]=media-contenuti-obsoleti
+
+[Section_documents]
+title=Documenti
+description=Gestisce la documentazione prodotta e pubblicata dal Comune come atti, progetti, bandi, concorsi e modulistica.
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=amministrazione-documenti-e-dati-accordi-tra-enti
+tags[]=amministrazione-documenti-e-dati-atti-normativi
+tags[]=amministrazione-documenti-e-dati-documenti-albo-pretorio
+tags[]=amministrazione-documenti-e-dati-documenti-attivita-politica
+tags[]=amministrazione-documenti-e-dati-documenti-di-programmazione-e-rendicontazione
+tags[]=amministrazione-documenti-e-dati-documenti-funzionamento-interno
+tags[]=amministrazione-documenti-e-dati-documenti-tecnici-di-supporto
+tags[]=amministrazione-documenti-e-dati-modulistica
+tags[]=media-immagini
+tags[]=media-images
+tags[]=argomenti
+
+[Section_news]
+title=Novità
+description=Gestione di avvisi, comunicati stampa e notizie
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=novita-avvisi
+tags[]=novita-comunicati
+tags[]=novita-notizie
+tags[]=media-immagini
+tags[]=media-images
+tags[]=argomenti
+
+[Section_services]
+title=Servizi
+description=Gestione delle schede di servizio
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=servizi
+tags[]=classificazioni-canali-digitali
+tags[]=classificazioni-cosa-puoi-richiedere
+tags[]=classificazioni-costi-e-tariffe
+tags[]=classificazioni-orari-servizi
+tags[]=media-immagini
+tags[]=media-images
+tags[]=argomenti
+
+[Section_dataset]
+title=Dataset
+description=
+link_label=Specifiche OpenAPI
+tags[]
+tags[]=amministrazione-documenti-e-dati-dataset
+tags[]=dataset
+tags[]=classificazioni-estensioni-temporali-dei-dataset
+tags[]=argomenti
+
+[Section_transparency]
+title=Amministrazione trasparente
+description=
+link_label=Specifiche OpenAPI
+tags[]=trasparenza
+tags[]=amministrazione-trasparente-bandi-di-gara-e-contratti-informazioni-sulle-singole-procedure-in-formato-tabellare
+tags[]=argomenti
 */ ?>
