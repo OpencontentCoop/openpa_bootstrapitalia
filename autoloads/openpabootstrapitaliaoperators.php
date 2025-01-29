f447e1db7e153a21fb16a1b4ab609d8f6e639ad0
Luca Realdi <luca.realdi@opencontent.it>
2025-01-29 12:15:19 +0100

    Nasconde l'interfaccia di edit (vuota) delle relazioni inversa

--- autoloads/openpabootstrapitaliaoperators.php
+++ autoloads/openpabootstrapitaliaoperators.php
@@ -1944,6 +1944,9 @@ class OpenPABootstrapItaliaOperators
             $contentObjectAttributeMap = [];
             $hiddenObjectAttributeMap = [];
             $sortMapper = [];
+            $hideDataTypeStrings = [
+                OpenPAReverseRelationListType::DATA_TYPE_STRING
+            ];
             foreach ($contentObjectAttributes as $attribute) {
                 $classAttribute = $attribute->contentClassAttribute();
                 $attributeCategory = $classAttribute->attribute('category');
@@ -1951,7 +1954,7 @@ class OpenPABootstrapItaliaOperators
                 $sortMapper[$classAttribute->attribute('placement')] = $attributeIdentifier;
                 if ($attributeCategory === 'hidden') {
                     $hiddenObjectAttributeMap[$attributeIdentifier] = $attribute;
-                } else {
+                } elseif (!in_array($attribute->attribute('data_type_string'), $hideDataTypeStrings)) {
                     $contentObjectAttributeMap[$attributeIdentifier] = $attribute;
                 }
             }
