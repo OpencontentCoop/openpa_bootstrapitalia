7708b1376592bf1f8ff5ecae882b9db51d0ab386
Marco Zampetti <marco.zampetti@opencitylabs.it>
2025-03-06 17:00:01 +0100

    Aggiunge le traduzioni per il calendario

--- translations/ita-IT/translation.ts
+++ translations/ita-IT/translation.ts
@@ -4,6 +4,10 @@
   <context>
     <name>bootstrapitalia</name>
     <message>
+      <source>View all events</source>
+      <translation>vedi tutti</translation>
+    </message>
+    <message>
       <source>This site uses technical, analytics and third-party cookies.</source>
       <translation>Questo sito utilizza cookie tecnici, analytics e di terze parti.</translation>
     </message>
