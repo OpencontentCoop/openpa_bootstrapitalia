42e1efd22c06eb6f33666c95bb0cec53176e3f7d
Marco Zampetti <marco.zampetti@opencitylabs.it>
2025-08-28 10:15:45 +0200

    Configurazione componente timeline

--- settings/block.ini.append.php
+++ settings/block.ini.append.php
@@ -25,6 +25,7 @@ AllowedTypes[]=OpendataRemoteContents
 AllowedTypes[]=Ricerca
 AllowedTypes[]=OpendataQueriedContents
 AllowedTypes[]=SearchPeopleByRole
+AllowedTypes[]=Timeline
 
 [Singolo]
 Name=Oggetto singolo
@@ -717,4 +718,13 @@ Wide[]
 Wide[]=default
 CanAddShowAllLink=disabled
 CanAddIntroText=disabled
+
+[Timeline]
+Name=Timeline
+NumberOfValidItems=20
+NumberOfArchivedItems=0
+ManualAddingOfItems=enabled
+ViewList[]
+ViewList[]=default
+ViewName[default]=Default
 */ ?>
