0b1f577da720c9c54c44b2c65af2511784fee0d3
Marco Zampetti <marco.zampetti@opencitylabs.it>
2026-02-20 16:48:20 +0100

    Annuncia agli screen reader l'errore di validazione nei campi testuali della prenotazione appuntamento

--- design/bootstrapitalia2/javascript/jquery.booking.js
+++ design/bootstrapitalia2/javascript/jquery.booking.js
@@ -1525,11 +1525,11 @@ var CodiceFiscale=function(A){var O={};function I(E){if(O[E])return O[E].exports
     },
 
     showInputError: function (input) {
-      input.parent().find('.form-text').addClass('text-warning').addClass('font-weight-bold')
+      input.parent().find('.form-text').addClass('text-warning').addClass('font-weight-bold').attr('role', 'alert')
     },
 
     hideInputError: function (input) {
-      input.parent().find('.form-text').removeClass('text-warning').removeClass('font-weight-bold')
+      input.parent().find('.form-text').removeClass('text-warning').removeClass('font-weight-bold').removeAttr('role')
     },
 
     makeSummary: function () {
