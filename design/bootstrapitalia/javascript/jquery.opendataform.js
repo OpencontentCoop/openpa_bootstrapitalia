93ee33a7fd9ad35d6fb566c28800e987648ffeaf
Marco Zampetti <marco.zampetti@opencitylabs.it>
2026-08-21 09:18:55 +0200

    Riscrive via flyimg le immagini nel form Alpaca del bottone Modifica (opendataform)
    Aggiunge l'helper OpenPAFlyImg (invariato rispetto alle copie già presenti in
    jquery.opendatabrowse.js) e un postRender in tutti e 3 i punti di jquery.
    opendataform.js che costruiscono alpacaOptions (opendataForm/opendataFormEdit,
    opendataFormView, opendataFormDelete), cosi' le thumbnail mostrate nel form del
    bottone "Modifica" (matita) e negli altri due entry point dello stesso plugin
    passano dall'URL diretto dello storage al proxy flyimg.

--- design/bootstrapitalia/javascript/jquery.opendataform.js
+++ design/bootstrapitalia/javascript/jquery.opendataform.js
@@ -1,3 +1,54 @@
+var OpenPAFlyImg = (function ($) {
+    var config = null;
+
+    function loadConfig() {
+        if (config !== null) {
+            return;
+        }
+        config = {enabled: false};
+        $.ajax({
+            url: '/ezjscore/call/ezjscflyimg::config',
+            async: false,
+            dataType: 'json'
+        }).done(function (data) {
+            if (data && data.content && data.content.enabled) {
+                config = data.content;
+            }
+        });
+    }
+
+    function rewrite(url, alias) {
+        if (!url) {
+            return url;
+        }
+        loadConfig();
+        if (!config.enabled) {
+            return url;
+        }
+        var filter = config.filters[alias] || config.filters.reference;
+        var filters = ['rf_1'];
+        if (config.defaultFilter) {
+            filters.push(config.defaultFilter);
+        }
+        filters.push('w_' + filter.w);
+        filters.push('h_' + filter.h);
+
+        var sourceUrl = url;
+        if (config.backendBaseUrl) {
+            var a = document.createElement('a');
+            a.href = url;
+            sourceUrl = url.replace(a.host, config.backendBaseUrl);
+            if (config.backendBaseScheme) {
+                sourceUrl = sourceUrl.replace(a.protocol.replace(':', ''), config.backendBaseScheme);
+            }
+        }
+
+        return config.baseUrl + '/' + filters.join(',') + '/' + encodeURIComponent(sourceUrl);
+    }
+
+    return {rewrite: rewrite};
+})(jQuery);
+
 // bug in DateTime field?
 Alpaca.defaultDateFormat = "DD/MM/YYYY";
 Alpaca.defaultTimeFormat = "HH:mm";
@@ -159,6 +210,12 @@ Alpaca.registerConnectorClass("opendataform", OpenContentOcopendataConnector);
                                 }
                             }
                         }
+                    },
+                    "postRender": function (field) {
+                        field.getFieldEl().find('img').each(function () {
+                            var $img = $(this);
+                            $img.attr('src', OpenPAFlyImg.rewrite($img.attr('src'), 'small'));
+                        });
                     }
                 }, options.alpaca);
 
@@ -251,6 +308,12 @@ Alpaca.registerConnectorClass("opendataform", OpenContentOcopendataConnector);
                                 }
                             }
                         }
+                    },
+                    "postRender": function (field) {
+                        field.getFieldEl().find('img').each(function () {
+                            var $img = $(this);
+                            $img.attr('src', OpenPAFlyImg.rewrite($img.attr('src'), 'small'));
+                        });
                     }
                 }, options.alpaca);
 
@@ -352,6 +415,12 @@ Alpaca.registerConnectorClass("opendataform", OpenContentOcopendataConnector);
                                 }
                             }
                         }
+                    },
+                    "postRender": function (field) {
+                        field.getFieldEl().find('img').each(function () {
+                            var $img = $(this);
+                            $img.attr('src', OpenPAFlyImg.rewrite($img.attr('src'), 'small'));
+                        });
                     }
                 }, options.alpaca);
 
